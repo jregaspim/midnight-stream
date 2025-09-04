c44b416e234a4f9b7cae0fafe42c191bf9d4d885
Midnight <jhumarregaspim@gmail.com>
2025-09-04 23:14:36 +0800

    add header and footer initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header></Header>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="logo">Midnight Stream</h1>
+      <h2 className="app-subttile">
+        It's time for popcorn! Find your next movie here.
+      </h2>
+    </div>
+  );
+}
