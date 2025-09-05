b47240cac1c839fcc19263c60bbf87ff4b33ba0d
Midnight <jhumarregaspim@gmail.com>
2025-09-06 02:02:11 +0800

    add errorhandling for image

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -13,10 +13,19 @@ interface MovieCardProps {
   movie: Movie;
 }
 export default function MovieCard({ movie }: MovieCardProps) {
+  const handleImageError = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    e.currentTarget.src = "images/default.jpg";
+  };
   return (
     <div>
       <div key={movie.id} className="movie-card">
-        <img src={`images/${movie.image}`} alt={movie.title} />
+        <img
+          src={`images/${movie.image}`}
+          alt={movie.title}
+          onError={handleImageError}
+        />
         <div className="movie-card-info">
           <h3 className="movei-card-title">{movie.title}</h3>
           <h3 className="movei-card-genre">{movie.genre}</h3>
